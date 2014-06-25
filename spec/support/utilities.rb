48d241f9e35cac5b492adedca8e5aa8cd757f84b
jflohre <jacobflohre@gmail.com>
2014-06-24 22:09:06 -0400

    Start of 5.4 user Signup

---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+#anything in support is automatically loaded by spec
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
